dc1dc2b537675859b8ce38950e161e3b41a8e8a7
AbdurRob <abdurrobr34l@gmail.com>
2025-11-18 00:59:42 +0600

    Completed brands section

--- Components/Brands.jsx
+++ Components/Brands.jsx
@@ -1,39 +1,66 @@
-import React from 'react';
-import 'swiper/css';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import amazon from '../src/assets/brands/amazon.png';
-import amazon_vector from '../src/assets/brands/amazon_vector.png';
-import casio from '../src/assets/brands/casio.png';
-import moonstar from '../src/assets/brands/moonstar.png';
-import randstad from '../src/assets/brands/randstad.png';
-import star from '../src/assets/brands/star.png';
-import start_people from '../src/assets/brands/start_people.png';
-import { Autoplay } from 'swiper/modules';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
 
-const brandLogos = [amazon, amazon_vector, casio, moonstar, randstad, star, start_people];
+import amazon from "../src/assets/brands/amazon.png";
+import amazon_vector from "../src/assets/brands/amazon_vector.png";
+import casio from "../src/assets/brands/casio.png";
+import moonstar from "../src/assets/brands/moonstar.png";
+import randstad from "../src/assets/brands/randstad.png";
+import star from "../src/assets/brands/star.png";
+import start_people from "../src/assets/brands/start_people.png";
+
+const brandLogos = [
+  amazon,
+  amazon_vector,
+  casio,
+  moonstar,
+  randstad,
+  star,
+  start_people
+];
 
 const Brands = () => {
-    return (
-        <Swiper
-            loop={true}
-            slidesPerView={4}
-            centeredSlides={true}
-            spaceBetween={30}
-            grabCursor={true}
-            modules={[Autoplay]}
-            autoplay={{
-                delay: 1000,
-                disableOnInteraction: false,
-            }}
-        >
-            {
-                brandLogos.map((logo, index) => <SwiperSlide key={index}>
-                    <img src={logo} alt="" />
-                </SwiperSlide>)
-            }
+  return (
+    <div className="w-full py-6 bg-transparent overflow-hidden">
 
-        </Swiper>
-    );
+<div>
+  <h2 className="font-extrabold text-[32px] text-center">We've helped thousands of sales teams</h2>
+</div>
+
+      <Swiper
+        modules={[Autoplay]}
+        loop={true}
+        grabCursor={false}
+        allowTouchMove={false}
+        slidesPerView={2}
+        speed={5000}
+        autoplay={{
+          delay: 0,
+          disableOnInteraction: false,
+        }}
+        freeMode={true}
+        freeModeMomentum={false}
+        className="mySwiper flex items-center mt-5 lg:mt-10"
+        breakpoints={{
+          480: { slidesPerView: 3 },
+          768: { slidesPerView: 4 },
+          1024: { slidesPerView: 6 },
+        }}
+      >
+        {brandLogos.concat(brandLogos).map((logo, index) => (
+          <SwiperSlide key={index} className="flex justify-center items-center">
+            <img
+              src={logo}
+              alt="brand logo"
+              className="w-24 h-auto opacity-80 hover:opacity-100 transition-all"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 };
 
 export default Brands;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import Banner from '../../Components/Banner';
-import Services from '../../Components/Services';
 import Works from '../../Components/Works';
-// import Brands from '../../Components/Brands';
+import Services from '../../Components/Services';
+import Brands from '../../Components/Brands';
 
 const Home = () => {
   return (
     <div className='flex flex-col gap-6 sm:gap-10 md:gap-14 lg:gap-20 2xl:gap-[100px]'>
       <Banner></Banner>
-      {/* <Brands></Brands> */}
       <Works></Works>
       <Services></Services>
+      <Brands></Brands>
     </div>
   );
 };

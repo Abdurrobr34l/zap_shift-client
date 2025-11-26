2314e7d38366ce7f293af69630fdf6279238bb5c
AbdurRob <abdurrobr34l@gmail.com>
2025-11-26 23:48:23 +0600

    Showing payment details

--- src/Pages/DashboardPages/Payment.jsx
+++ src/Pages/DashboardPages/Payment.jsx
@@ -1,9 +1,29 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { useParams } from 'react-router';
+import useAxios from '../../Hooks/useAxios';
+import Loading from '../../../Components/Loading';
 
 const Payment = () => {
+  const axiosSecure = useAxios();
+
+  const { parcelId } = useParams();
+  const { isLoading, data } = useQuery({
+    queryKey: ["[parcels", parcelId],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/parcels/${parcelId}`);
+      return res.data;
+    }
+  })
+
+  if (isLoading) {
+    return <Loading></Loading>
+  }
+
   return (
     <div>
-      Payment
+      <h2>Please pay for: {data.parcel_name}</h2>
+      <button className="btn btn-accent">Pay</button>
     </div>
   );
 };
